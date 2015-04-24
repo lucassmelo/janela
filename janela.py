e1b133127ef181de070cd4fd912a24627516b114
lucassm <lucas.silveiramelo@gmail.com>
2015-04-23 22:49:13 -0300

    nova arquitetura

--- janela.py
+++ janela.py
@@ -1,11 +1,29 @@
-from PySide import QtGui
 import sys
+from PySide import QtGui
 
-app = QtGui.QApplication(sys.argv)
 
-wid = QtGui.QWidget()
-wid.resize(250, 150)
-wid.setWindowTitle('Janela')
-wid.show()
+class Example(QtGui.QWidget):
 
-sys.exit(app.exec_())
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        label = QtGui.QLabel('Hello World!', self)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Janela')
+        self.show()
+
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
